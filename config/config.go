17105b0f1f14f9a879aa4dfad4fca05aebe90e24
ugulino@hotmail.com <@R37chlo16>
2024-11-08 23:20:17 -0300

    refactor: add config

---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+// LoadAWSConfig carrega as configurações da AWS
+func LoadAWSConfig(region string) aws.Config {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+	return cfg
+}
